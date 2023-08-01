084f128b193fc3a993f394f9bf4cfacaaf2e9daa
Huong Hoang <hoangminhhuong1408@gmail.com>
2023-08-02 01:29:27 +0700

    design edit note page

--- src/pages/EditNote.jsx
+++ src/pages/EditNote.jsx
@@ -1,5 +1,22 @@
+import { IoIosArrowBack } from 'react-icons/io';
+import { RiDeleteBin6Line } from 'react-icons/ri';
+import { Link } from 'react-router-dom';
+
 export default function EditNote() {
 	return (
-		<div>EditNote</div>
-	)
+		<section>
+			<header className='create-note__header'>
+				<Link to='/' className='btn'>
+					<IoIosArrowBack />
+				</Link>
+				<button className='btn lg danger'>
+					<RiDeleteBin6Line />
+				</button>
+			</header>
+			<form className='create-note__form'>
+				<input type='text' autoFocus placeholder='Title ...' />
+				<textarea rows='20' placeholder='Note details ...'></textarea>
+			</form>
+		</section>
+	);
 }
