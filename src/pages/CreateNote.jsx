3c54f84d5efada27558edc7d4a71f125d0908db8
Huong Hoang <hoangminhhuong1408@gmail.com>
2023-08-02 01:25:31 +0700

    design create note page

--- src/pages/CreateNote.jsx
+++ src/pages/CreateNote.jsx
@@ -1,5 +1,19 @@
+import { Link } from 'react-router-dom';
+import { IoIosArrowBack } from 'react-icons/io';
+
 export default function CreateNote() {
 	return (
-		<div>CreateNote</div>
-	)
+		<section>
+			<header className='create-note__header'>
+				<Link to='/' className='btn'>
+					<IoIosArrowBack />
+				</Link>
+				<button className="btn lg primary">Save</button>
+			</header>
+			<form className="create-note__form">
+				<input type="text" autoFocus placeholder='Title ...' />
+				<textarea rows="20" placeholder='Note details ...'></textarea>
+			</form>
+		</section>
+	);
 }
